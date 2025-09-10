75045d45352b1f34642ab4c6b893b94e7ce06b36
emrelax1 <emre1153ztrk@gmail.com>
2025-09-10 13:59:51 +0300

    feature : Implement functions to handle input values for weight, temperature, volume, and length

--- javascript.js
+++ javascript.js
@@ -1,18 +1,8 @@
-/* var selectedType = "agirlik";
-
-function setSelectionValue(newValue) {
-  console.log(newValue.value);
-  console.log(newValue.options[newValue.selectedType], text);
-  selectedType = newValue.value;
-}
- */
-
 function setSelectionValue(newValue) {
   const dropdown = document.getElementById("type");
   const value = dropdown.value;
   const text = dropdown.options[dropdown.selectedIndex].text;
-  /* console.log(value);
-  console.log(text); */
+
   if (value == null) {
     document.getElementById("agirlikdivi").style.display = "block";
     document.getElementById("sicaklikdivi").style.display = "none";
@@ -45,43 +35,23 @@ function setSelectionValue(newValue) {
   }
 }
 
-function getInputValueAgirlik(inValue) {
+function getInputValueAgirlik(ValueAgirlik) {
   const input1 = document.getElementById("agirliktipi");
-  const inputValue1 = input1.value;
-  const input2 = document.getElementById("agirliktipi2");
-  const inputValue2 = input2.value;
-  const input3 = document.getElementById("agirliktipi3");
-  const inputValue3 = input3.value;
-
-  console.log(inputValue1);
-  console.log(inputValue2);
-
-  console.log(inputValue3);
-
-  /*  if (changed === "input1") {
-    console.log("changed çalışıyor");
-    inputValue2 = (45359237 * inputValue1) / 45359237000;
-    inputValue3 = (45359237 * inputValue1) / 100000;
-  }
-  return { inputValue1, inputValue2, inputValue3 }; */
-  /* 45359237 * inputValue1 = 45359237000 * inputValue2 = 100000 * inputValue3; */
+  const inputValueAgirlik = input1.value;
+  console.log(inputValueAgirlik);
 }
-function updateValues(changed, value) {
-  let input1, input2, input3;
-
-  if (changed === "input1") {
-    input1 = value;
-    input2 = (45359237 * input1) / 45359237000;
-    input3 = (45359237 * input1) / 100000;
-  } else if (changed === "input2") {
-    input2 = value;
-    input1 = (45359237000 * input2) / 45359237;
-    input3 = (45359237000 * input2) / 100000;
-  } else if (changed === "input3") {
-    input3 = value;
-    input1 = (100000 * input3) / 45359237;
-    input2 = (100000 * input3) / 45359237000;
-  }
-
-  return { input1, input2, input3 };
+function getInputValueSicaklik(ValueSicaklik) {
+  const input2 = document.getElementById("sicakliktipi");
+  const inputValueSicaklik = input2.value;
+  console.log(inputValueSicaklik);
+}
+function getInputValueUzunluk(ValueUzunluk) {
+  const input3 = document.getElementById("uzunluktipi");
+  const inputValueUzunluk = input3.value;
+  console.log(inputValueUzunluk);
+}
+function getInputValueHacim(ValueHacim) {
+  const input4 = document.getElementById("hacimtipi");
+  const inputValueHacim = input4.value;
+  console.log(inputValueHacim);
 }
